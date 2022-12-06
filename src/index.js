c871af7bb046782dba2d0f63e726c88e19e85d00
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2022-12-06 08:19:22 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  console.log("loaded");
 });
+const taskForm = document.getElementById("create-task-form");
+console.log(taskForm);
+const descBox = document.getElementById("new-task-description");
+console.log(descBox);
+const taskContainer = document.getElementById("tasks");
+console.log(taskContainer);
+
+taskForm.addEventListener("submit", createNewTask);
+
+function createNewTask(e) {
+  e.preventDefault();
+  const task = e.target.querySelector("#new-task-description").value;
+  renderTask(task);
+  reset;
+}
+
+function renderTask(e) {
+  const li = document.createElement("li");
+  li.textContent = `${e} `;
+  taskContainer.append(li);
+}
